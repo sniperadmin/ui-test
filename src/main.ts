bc9850020c9af1e9db24d89876ed426eaf0d4c32
Nasr Galal <nasrbue@gmail.com>
2021-12-30 16:10:20 +0200

    refactor: remove unwanted plugin

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
 import './assets/scss/main.scss'
 import Handlebars from "handlebars";
-// import Remus from 'remusjs';
+import iconExercise from '@/assets/images/icon-exercise.svg';
 
 let app = document.querySelector<HTMLDivElement>("#app")!
-
-
-// let remus = new Remus({
-//   element: '.remus', // selector for main Remus container
-//   height: null // height value in pixels if you want it to be fixed - optional
-// });
 
 let templateString = document.querySelector("#entry-template")!.innerHTML
 const template = Handlebars.compile(templateString);
